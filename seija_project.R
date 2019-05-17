a80c8be09086915898eafe4ffceb0e47c73bf72e
Emmett Leyne <emmettleyne@gmail.com>
2019-05-17 16:21:46 +1000

    Adding traceys project

--- seija_project.R
+++ seija_project.R
@@ -2,30 +2,49 @@ library(tidyverse)
 library(emmeans)
 library(multcompView)
 library(lmerTest)
+library(janitor)
 
 # Import Seija's data
+N1 <- read_csv('Comparison_Experiment_Kcl_v_K2SO4.csv')
+N1 <- clean_names(N1, case = "snake")
+N1$extractant <- str_replace_all(N1$extractant,' ','')
 
-N_experiment <- read_csv('Comparison_Experiment_Kcl_v_K2SO4.csv')
 
-N_experiment_2 <- N_experiment %>% gather(key=N_type, value = amount, c(7,9,11))
+# Add in the treatment (fertiliser) intially omited from the dataset
+N1$treatment <- rep(c("urea", 'water'), each = 12, times=3)
+N1$rep_id <- with(N1, interaction(soil, treatment, extractant))
+str(N1)
 
-ggplot(N_experiment_2, aes(Soil, amount, colour = Extractant)) +
+# Gather the adjusted data to make it tidy
+N2 <- N1 %>% gather(key=n_type, value = n_amount, c(7,9,11))
+
+ggplot(N1, aes(soil, log(nh3n_adjusted), colour=extractant))+
   geom_boxplot() +
-  facet_wrap(~N_type, scales = "free")
+  facet_wrap(~treatment)
 
-ggplot(N_experiment, aes(Soil, NH3N_adjusted, colour = Extractant)) +
-  geom_boxplot()
+ggplot(N1, aes(treatment, log(nh3n_adjusted), colour=extractant))+
+  geom_boxplot() +
+  facet_wrap(~soil)
 
-ggplot(N_experiment, aes(Soil, log(NH3N_adjusted), colour = Extractant)) +
-  geom_boxplot()
 
-# Remove water
+ggplot(N2, aes(soil, log(n_amount), colour = extractant)) +
+  geom_boxplot() +
+  facet_wrap(~n_type)
 
-N_experiment_3 <- subset(N_experiment, Extractant != 'H2O')
+# Remove water from original data to look at the two extractant treatments without the confounding of the water
 
-lm1 <- lm(log(NH3N_adjusted)~Soil*Extractant, N_experiment_3)
-anova(lm1)
-emmeans(lm1, pairwise~Extractant|Soil, type = 'response')
-emmeans(lm1, revpairwise~Extractant|Soil, type = 'response')
+N3 <- subset(N1, extractant != 'H2O')
 
-plot(lm1, 1:2)
+#Additive model
+lm2 <- lmer(log(nh3n_adjusted)~extractant+treatment+soil+(1|rep_id), N3)
+anova(lm2)
+summary(lm2)
+
+#Factorial model (treatment exluded as additive model showed little effect)
+lm3 <- lmer(log(nh3n_adjusted)~extractant*soil+(1|rep_id), N3)
+anova(lm3)
+summary(lm3)
+emmeans(lm3, pairwise~extractant|soil, type = 'response')
+emmeans(lm3, revpairwise~extractant|soil, type = 'response') #Reverse pairwise will swap the results over (ie, show an increase instead of a decrease)
+
+plot(lm3)
---
+++ tracey_project.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(emmeans)
+library(multcompView)
+library(lmerTest)
+library(janitor)
+
+cotton <- read_csv('tracey_BiologicalTrialData1.csv') %>% 
+  clean_names('snake')
+cotton$weekf <- cotton$week
+str(cotton)
+factcols <- c('plot', 'transect','map_kg_ha','nitrogen_kg_ha', 'weekf')
+cotton[factcols] <- lapply(cotton[factcols], factor)
+
+plantcols <- c('plant_1', 'plant_2','plant_3', 'plant_4','plant_5','plant_6','plant_7')
+with(cotton, table(map_kg_ha, nitrogen_kg_ha, treatment))
+
+cotton2 <- gather(cotton, key = 'plant_number', value = 'plant_height', plantcols)
+
+ggplot(cotton2, aes(weekf, plant_height, colour = treatment)) + 
+  geom_boxplot()
+
+ggplot(cotton2, aes(week, plant_height, colour = treatment)) + 
+  geom_point() +
+  geom_smooth(alpha=.2)
+
+ggplot(cotton2, aes(week, plant_height, colour = treatment)) + 
+  geom_point() +
+  geom_smooth(alpha=.2)+
+  facet_wrap(~nitrogen_kg_ha*map_kg_ha)
+
+lm1 <- lmer(plant_height~treatment+map_kg_ha+nitrogen_kg_ha+(1|transect) + (1|plot), cotton2)
+anova(lm1)
+summary(lm1)
+
+lm2 <- lmer(plant_height~map_kg_ha+nitrogen_kg_ha+treatment+weekf+
+              treatment*weekf + treatment*nitrogen_kg_ha +
+              treatment*map_kg_ha+(1|transect) + (1|plot), cotton2)
+anova(lm2)
+
+# A shorter way of the lm2
+lm3 <- lmer(plant_height~weekf*(nitrogen_kg_ha + treatment + map_kg_ha)+
+              (1|transect) + (1|plot), cotton2)
+anova(lm3)
